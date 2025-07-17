3fab21450d90b2c17e075cc2333b2dcc44c59e9f
Rodrigo Ramos <rodrigo.oliveira@camtecnologia.com.br>
2025-07-17 17:00:21 -0300

    feat: implemented authentication

--- api/app.js
+++ api/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
@@ -7,6 +7,7 @@ import postRoute from "./routes/post.route.js";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user', userRoute);
 app.use('/api/post', postRoute);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,75 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
-    // DB operations
-    const { username, email, password } = req.body
-    
-    const hashPassword = await bcrypt.hash(password, 10)
-    console.log(hashPassword)
+  const { username, email, password } = req.body;
 
-    res.json({ message: hashPassword })
-}
+  try {
+    const hashPassword = await bcrypt.hash(password, 10);
 
-export const login = (req, res) => {
-    // DB operations
-}
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashPassword,
+      },
+    });
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error creating user" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: age,
+      }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login successful!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
 
 export const logout = (req, res) => {
-    // DB operations
-}
+  res.clearCookie("token").status(200).json({ message: "Logout successful!" });
+};
